bb4fe326b88f51678dc292910ee7ca101202a7a9
Alexandru Toader <alextove02@gmail.com>
2025-01-15 19:48:54 +0200

    added tljh install script + configurations

---
+++ littlest-jupyterhub/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# Load configured variables
+
+export $(cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | xargs)
+
+# 1. Update system and install prerequisites
+
+echo "Installing The Littlest JupyterHub prerequisites..."
+sudo apt install python3 python3-dev git curl
+
+# 2. Install TLJH
+
+echo "Installing The Littlest JupyterHub..."
+curl -L https://tljh.jupyter.org/bootstrap.py | sudo python3 - --admin "$ADMIN_USER:$ADMIN_PASSWORD"
+
+# 3. Configure authentication
+
+# Set up native authentication
+echo "Setting up native authentication..."
+sudo tljh-config set auth.type nativeauthenticator.NativeAuthenticator
+
+# Reload configurations after setting authentication
+sudo tljh-config reload
+
+# 4. Install additional python packages that will be available for all users
+
+echo "Installing additional Python packages..."
+sudo -E pip install -r requirements.txt
+
+# 6. Create a shared data folder where an admin can add utility files that will be available for new users
+# as read-only.
+
+echo "Creating shared folder..."
+sudo mkdir -p /srv/data/shared_data
+cd /etc/skel
+sudo ln -s /srv/data/shared_data shared_data
+# (Optional) Download data into /srv/data/my_shared_data_folder
